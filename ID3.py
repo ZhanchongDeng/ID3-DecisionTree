10f1847637dd613ce9c3ae0a0317fb08c6524423
Bobby <ZhanchongDeng@github.com>
2020-02-19 16:55:43 -0800

    Done with pruning

--- ID3.py
+++ ID3.py
@@ -6,6 +6,7 @@ Contains all methods for a ID3 Decision Tree.
 import numpy as np
 import pandas as pd
 from scipy.stats import entropy
+from scipy.stats import mode
 
 '''
 Return numpy arrays representing the dataset.
@@ -26,10 +27,13 @@ Node for Decision Tree
 
 
 class idNode():
-    # Fields: yes(idNode), no(idNode), threshold(float), feature(int)
+    # how many data in this node
+    numPoints = 0
+    data=[]
     # Branches
     yes = None
     no = None
+    parent = None
     # Is is a leaf?
     isLeaf = False
     # How to get here
@@ -40,12 +44,20 @@ class idNode():
     # For leaves
     rule = -1
     
+
+    
+    def pruneNode(self):
+        self.yes = None
+        self.no = None
+        self.isLeaf = True
+        self.rule = mode(self.data[:,-1])[0][0]
+    
     def toString(self):
         if not self.isLeaf:
-            return "(" + self.route + ")" + "[is feature at " + str(self.feature+1) + " >= " + str(self.threshold) \
-                   + "?]\n"
+            return "(" + self.route + ")" + "[is feature at " + str(self.feature+1) + " < " + str(self.threshold) \
+                   + "?](num_data:" + str(self.numPoints) + ")\n"
         else:
-            return "(" + self.route + ")" + "[" + str(self.rule) + "]\n"
+            return "(" + self.route + ")" + "[" + str(self.rule) + "](num_data:" + str(self.numPoints) + ")\n"
 
         
 '''
@@ -59,18 +71,22 @@ class id3():
         self.root = idNode()
         print("Created a new ID3 tree")
 
+    # Training
     def fit(self, training_data):
         self.build(self.root, training_data)
 
     def build(self, node, v):
-        # Stop if it is pure
+        # Base case: Stop if it is pure
         if self.isPure(v):
             # Make it a leaf node.
             node.isLeaf = True
             node.rule = v[0][-1]
+            node.numPoints = len(v)
             
         # Parse them according to H(entropy)
         else:
+            node.numPoints = len(v)
+            node.data = v
             # Pick a feature f and threshold t
             node.feature = 0
             minEntropy = float("inf")
@@ -87,35 +103,25 @@ class id3():
                     node.feature = thisfeature
                     node.threshold = result[0]
             # Split them according to the rule
-            v_yes = v[v[:,node.feature] >= node.threshold]
-            v_no = v[v[:,node.feature] < node.threshold]
+            v_yes = v[v[:,node.feature] < node.threshold]
+            v_no = v[v[:,node.feature] >= node.threshold]
             # Create new nodes
             node.yes = idNode()
             node.yes.route = "yes"
+            node.yes.parent = node
             self.build(node.yes, v_yes)
             node.no = idNode()
             node.no.route = "no"
+            node.no.parent = node
             self.build(node.no, v_no)
 
     def isPure(self, vec):
         return len(np.unique(vec[:, -1])) == 1
 
     def generateThreshold(self, feature):
+        # Generate based solemnly on the feature vector
         distinct_val = np.sort(np.unique(feature[:,0]))
-        if len(distinct_val) == 0:
-            print(feature)
         return (distinct_val[:-1] + distinct_val[1:]) / 2
-    # def generateThreshold(self, feature):
-    #     # Sort them according to given feature
-    #     sortedFeatures = feature[feature[:, 0].argsort()]
-    #     # Determine where the labels switched
-    #     changed = sortedFeatures[:-1, -1] != sortedFeatures[1:, -1]
-    #     # Get the before and after value at those swithces
-    #     before = sortedFeatures[:-1, 0]
-    #     after = sortedFeatures[1:, 0]
-    #     # Return a list of the threshold by this feature
-    #     return np.unique((before[changed] + after[changed]) / 2)
-
     
     def maxIG(self, list_of_threshold, v_at_f):
         # Initialize as the first threshold
@@ -132,8 +138,8 @@ class id3():
 
     def entropy_with_threshold(self, threshold, v_at_f):
         # Slice v according to the given threshold
-        v_yes = v_at_f[v_at_f[:,0] >= threshold]      
-        v_no = v_at_f[v_at_f[:,0] < threshold]
+        v_yes = v_at_f[v_at_f[:,0] < threshold]      
+        v_no = v_at_f[v_at_f[:,0] >= threshold]
         # Calculate H(X|Z=yes)
         margin_yes = [ np.sum(v_yes[:,-1]==num_labels) / len(v_yes) for num_labels in np.unique(v_yes[:,-1])]
         h_yes = entropy(margin_yes)
@@ -143,21 +149,91 @@ class id3():
         # Calculate H(X|Z)
         return len(v_yes)/len(v_at_f) * h_yes + len(v_no)/len(v_at_f) * h_no
     
-    def toString(self):
-        tree_str = self.printTree(self.root, 0)
+    # Validation
+    def pruneTree(self, validation):
+        queue = [self.root]
+        while len(queue) > 0:
+            # Calculate Current Error
+            oldError = self.error(validation)
+            # Use list as a queue
+            curNode = queue.pop(0)
+            # Create a deep copy
+            original = idNode()
+            original = self.copyNode(curNode)
+            # Prune it
+            curNode.pruneNode()
+            # Compare errors
+            newError = self.error(validation)
+            # We screwed up, revert
+            if oldError < newError:
+                # Edge for root
+                if curNode.parent is None:
+                    self.root = original
+                    original.yes.parent = self.root
+                    original.no.parent = self.root
+                else:
+                    if curNode.route == "yes":
+                        curNode.parent.yes = original
+                    else:
+                        curNode.parent.no = original
+                if not original.yes.isLeaf:
+                    queue.append(original.yes)
+                if not original.no.isLeaf:
+                    queue.append(original.no)
+                    
+    def copyNode(self,node):
+        newNode = idNode()
+        newNode.numPoints = node.numPoints
+        newNode.data = np.copy(node.data)
+        newNode.yes = node.yes
+        newNode.no = node.no
+        newNode.parent = node.parent
+        newNode.isLeaf = node.isLeaf
+        newNode.route = node.route
+        newNode.threshold = node.threshold
+        newNode.feature = node.feature
+        newNode.rule = node.rule
+        return newNode       
+    
+    # Testing
+    def predict(self, features):
+        return np.apply_along_axis(self.predictOne, 1, features)
+    
+    def error(self, test_data):
+        return np.mean(self.predict(test_data[:,:-1]) != test_data[:,-1])
+    
+    def predictOne(self, v):
+        node = self.root
+        while not node.isLeaf:
+            # Yes branch
+            if v[node.feature] < node.threshold:
+                node = node.yes
+            else:
+                node = node.no
+        # This should never happen
+        if node.rule == -1:
+            print("There is something wrong with your tree")
+        return node.rule
+    
+    # Visualization
+    def printTree(self):
+        tree_str = self.printTreeR(self.root, 0, float("inf"))
         return tree_str
     
-    def printTree(self, curNode, level):
+    def printTreeAt(self, maxlevel):
+        return self.printTreeR(self.root, 0, maxlevel)
+    
+    def printTreeR(self, curNode, level, maxlevel):
         # do yourself
         curStr = '\t' * level + curNode.toString()
         # do yes
-        if not curNode.yes.isLeaf:
-            curStr += self.printTree(curNode.yes, level + 1)
+        if not curNode.yes.isLeaf and level+1 < maxlevel-1:
+            curStr += self.printTreeR(curNode.yes, level + 1, maxlevel)
         else:
             curStr += '\t' * (level + 1) + curNode.yes.toString()
         # do no
-        if not curNode.no.isLeaf:
-            curStr += self.printTree(curNode.no, level + 1)
+        if not curNode.no.isLeaf and level+1 < maxlevel-1:
+            curStr += self.printTreeR(curNode.no, level + 1, maxlevel)
         else:
             curStr += '\t' * (level + 1) + curNode.no.toString()
         return curStr
